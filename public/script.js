e9fa6357fc4f12c9ad8ba12472f6d1bd374b7794
ayush7800ishere <ayush7800ishere@gmail.com>
2022-07-24 03:18:38 +0530

    changed port to 3030

--- public/script.js
+++ public/script.js
@@ -24,7 +24,8 @@ navigator.mediaDevices.getUserMedia({
   })
 
   socket.on('user-connected', userId => {
-    setTimeout(connectToNewUser, 1000, userId, stream)
+    connectToNewUser(userId, stream)
+    // setTimeout(connectToNewUser, 1000, userId, stream)
   })
   // input value
   let text = $("input");
